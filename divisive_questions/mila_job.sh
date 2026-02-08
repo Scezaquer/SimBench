56a444a57ae68537fed06a4ea885a0596bad3720
Scezquer <scezaquer@gmail.com>
2026-02-08 18:36:34 -0500

    updates

--- divisive_questions/mila_job.sh
+++ divisive_questions/mila_job.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=SimBench_LoRAs
+#SBATCH --job-name=DivisiveQuestions
 #SBATCH --time=12:00:00
 #SBATCH --mem-per-cpu=16G
 #SBATCH --gres=gpu:a100:1
@@ -13,9 +13,9 @@ export LOCAL_WORKDIR=/home/mila/a/aurelien.buck-kaeffer/SimBench
 export HF_HUB_OFFLINE=1
 export HF_CACHE_LOCAL=$SCRATCH/HF-cache
 
-python divisive_questions/find_divisive_questions.py \
+python find_divisive_questions.py \
     --base_model "marcelbinz/Llama-3.1-Minitaur-8B" \
     --lora_dir "/home/mila/a/aurelien.buck-kaeffer/scratch/marcelbinz" \
-    --questions_file "divisive_questions/potential_questions.json" \
-    --output_file "results/divisive_questions_probabilities.json" \
+    --questions_file "potential_questions.json" \
+    --output_file "divisive_questions_probabilities.json" \
     --load_in_4bit
